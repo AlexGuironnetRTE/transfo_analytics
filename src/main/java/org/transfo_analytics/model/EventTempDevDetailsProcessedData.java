f2ea6b4987d7276e86d5c5e52f3823eed338433b
Alexandra Guironnet <alexandra.guironnet@rte-france.com>
2020-01-14 18:56:02 +0100

    Fixed missing index

--- src/main/java/org/transfo_analytics/model/EventTempDevDetailsProcessedData.java
+++ src/main/java/org/transfo_analytics/model/EventTempDevDetailsProcessedData.java
@@ -6,16 +6,17 @@ import java.util.List;
 
 public class EventTempDevDetailsProcessedData {
 
-    private List<String> labels; //X axis labels
+    private List<Long> labels; //X axis labels
     private List<String> label; //Name of dataset
     private List<List<Long>> data;
 
     public EventTempDevDetailsProcessedData(EventTempDevDetails eventTempDevDetails) {
         this.label = eventTempDevDetails.getColumns();
         this.data = Utilities.transpose(eventTempDevDetails.getData());
+        this.labels = eventTempDevDetails.getIndex();
     }
 
-    public List<String> getLabels() {
+    public List<Long> getLabels() {
         return labels;
     }
 
