9f831a8d5bf828f38e7af323f40082d58fd518cd
Alexandra Guironnet <alexandra.guironnet@rte-france.com>
2020-01-14 18:47:41 +0100

    Add necessary getters

--- src/main/java/org/transfo_analytics/model/EventTempDevDetailsProcessedData.java
+++ src/main/java/org/transfo_analytics/model/EventTempDevDetailsProcessedData.java
@@ -1,13 +1,9 @@
 package org.transfo_analytics.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.transfo_analytics.utils.Utilities;
 
 import java.util.List;
 
-@Getter
 public class EventTempDevDetailsProcessedData {
 
     private List<String> labels; //X axis labels
@@ -17,5 +13,17 @@ public class EventTempDevDetailsProcessedData {
     public EventTempDevDetailsProcessedData(EventTempDevDetails eventTempDevDetails) {
         this.label = eventTempDevDetails.getColumns();
         this.data = Utilities.transpose(eventTempDevDetails.getData());
+    }
+
+    public List<String> getLabels() {
+        return labels;
+    }
+
+    public List<String> getLabel() {
+        return label;
+    }
+
+    public List<List<Long>> getData() {
+        return data;
     }
 }
